0ccf3dd590373580f42defd472b5aa9774843b62
linusrud <rudbeck.linus@gmail.com>
2014-04-03 12:58:39 +0200

    :(

--- VetInte37/VetInte37/Login.h
+++ VetInte37/VetInte37/Login.h
@@ -142,6 +142,7 @@ namespace Forms {
 			// 
 			// MyForm
 			// 
+			this->AcceptButton = this->btnLogin;
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(364, 128);
