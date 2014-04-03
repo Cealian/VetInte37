20a6100d97d1610fc7e5f2561ea82fa578bc939d
linusrud <rudbeck.linus@gmail.com>
2014-04-03 13:10:57 +0200

    Menyfönster skapat

--- VetInte37/VetInte37/Form.h
+++ VetInte37/VetInte37/Form.h
@@ -53,6 +53,10 @@ namespace Forms{
 		DbProviderFactory^ fac;
 		DbConnection^ conn;
 		DbCommand^ cmd;
+	private: System::Windows::Forms::GroupBox^  grpStudent;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::GroupBox^  gprPersonal;
+	private: System::Windows::Forms::Button^  button2;
 
 
 
@@ -83,16 +87,64 @@ namespace Forms{
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->grpStudent = (gcnew System::Windows::Forms::GroupBox());
+			this->gprPersonal = (gcnew System::Windows::Forms::GroupBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->grpStudent->SuspendLayout();
+			this->gprPersonal->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// grpStudent
+			// 
+			this->grpStudent->Controls->Add(this->button1);
+			this->grpStudent->Location = System::Drawing::Point(12, 12);
+			this->grpStudent->Name = L"grpStudent";
+			this->grpStudent->Size = System::Drawing::Size(88, 100);
+			this->grpStudent->TabIndex = 0;
+			this->grpStudent->TabStop = false;
+			this->grpStudent->Text = L"Student";
+			// 
+			// gprPersonal
+			// 
+			this->gprPersonal->Controls->Add(this->button2);
+			this->gprPersonal->Location = System::Drawing::Point(106, 12);
+			this->gprPersonal->Name = L"gprPersonal";
+			this->gprPersonal->Size = System::Drawing::Size(89, 100);
+			this->gprPersonal->TabIndex = 0;
+			this->gprPersonal->TabStop = false;
+			this->gprPersonal->Text = L"Personal";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(6, 19);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Skriv tenta";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(6, 19);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 0;
+			this->button2->Text = L"Skapa tenta";
+			this->button2->UseVisualStyleBackColor = true;
 			// 
 			// Form2
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(451, 371);
+			this->ClientSize = System::Drawing::Size(205, 122);
+			this->Controls->Add(this->gprPersonal);
+			this->Controls->Add(this->grpStudent);
 			this->Name = L"Form2";
-			this->Text = L"Form2";
+			this->Text = L"Meny";
 			this->Load += gcnew System::EventHandler(this, &Form2::Form2_Load);
+			this->grpStudent->ResumeLayout(false);
+			this->gprPersonal->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
